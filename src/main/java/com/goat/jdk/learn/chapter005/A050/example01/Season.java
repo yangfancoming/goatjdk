03611ba67e10b0ea292bec6ec73ef0d4ebb6eec0
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-10-28 17:04:04 +0800

    学习 java基础 枚举类

---
+++ src/main/java/com/goat/jdk/learn/chapter005/A050/example01/Season.java
@@ -0,0 +1,12 @@
+package com.goat.jdk.learn.chapter005.A050.example01;
+
+/**
+ * Created by 64274 on 2018/9/11.
+ *
+ * @author 山羊来了
+ * @Description: TODO
+ * @date 2018/9/11---11:23
+ */
+public enum Season {
+    SPRING, SUMMER, FALL, WINTER
+}
---
+++ src/main/java/com/goat/jdk/learn/chapter005/A050/example03/TestNG2.java
@@ -0,0 +1,28 @@
+package com.goat.jdk.learn.chapter005.A050.example03;
+
+
+import org.junit.Test;
+
+/**
+     * @Description: 功能描述：
+     * @author: Goat
+     * @Param:
+     * @Return:
+     * @Date:   2018/9/3
+*/
+
+public class TestNG2 {
+
+    @Test
+    public void test0(){
+        System.out.println(Weekday2.FRI.getKey());
+        System.out.println(Weekday2.FRI.getValue());
+    }
+    @Test
+    public void test(){
+
+    }
+
+
+
+}
